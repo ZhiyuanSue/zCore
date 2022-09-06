fbca108ae204ab70e8f02d267db469fd70697f86
ZhiyuanSue <2262387848@qq.com>
2022-09-06 09:38:14 +0800

    try add pid ns

--- linux-object/src/namespace/pidns.rs
+++ linux-object/src/namespace/pidns.rs
@@ -2,10 +2,12 @@
 use super::*;
 
 pub type NsPid = u64;
+pub type NsTid = u64;
 pub struct PidNs
 {
     base:NsBase,
     pid_map:HashMap<KoID,NsPid>,
+    tid_map:HashMap<KoID,NsTid>,
     usr_ns:KoID,
 }
 impl NS for PidNs{
@@ -40,6 +42,7 @@ impl PidNs{
         let pidns=PidNs{
             base:NsBase::new(NSType::CLONE_NEWNS,parent),
             pid_map:HashMap::new(),
+            tid_map:HashMap::new(),
             usr_ns:usr_id,
         };
         pidns
@@ -79,7 +82,61 @@ impl PidNs{
             }
         }
     }
-    pub fn insert_pid(mut self,processer_id:KoID,namespace_pid:NsPid){
+    fn get_new_pid(&mut self)->NsPid{
+        0
+    }
+    fn get_new_tid(&mut self)->NsTid{
+        0
+    }
+    pub fn insert_pid(&mut self,processer_id:KoID){
+        //alloc a new pid in the current ns for this processer and insert
+        let namespace_pid=self.get_new_pid();
         self.pid_map.insert(processer_id, namespace_pid);
+        //insert to the parent recursively
+        //I don't know why my code seems strange. should I use '?' ?
+        let parent_ns=self.base.parent;
+        let nsmanager=NS_MANAGER.lock();
+        match parent_ns{
+            Some(ns_id)=>{
+                let ns_parent_enum=nsmanager.get_ns(ns_id);
+                match ns_parent_enum{
+                    Some(mutex_e)=>{
+                        let mut e=mutex_e.lock();
+                        match e.deref_mut(){
+                            NsEnum::PidNs(p)=>
+                            {
+                                p.insert_pid(processer_id);
+                            },
+                            _=>()}
+                    },None=>()}
+            },None=>()};
+    }
+    pub fn insert_tid(&mut self,thread_id:KoID){
+        //alloc a new pid in the current ns for this processer and insert
+        let namespace_tid=self.get_new_tid();
+        self.tid_map.insert(thread_id, namespace_tid);
+        //insert to the parent recursively
+        let parent_ns=self.base.parent;
+        let nsmanager=NS_MANAGER.lock();
+        match parent_ns{
+            Some(ns_id)=>{
+                let ns_parent_enum=nsmanager.get_ns(ns_id);
+                match ns_parent_enum{
+                    Some(mutex_e)=>{
+                        let mut e=mutex_e.lock();
+                        match e.deref_mut(){
+                            NsEnum::PidNs(p)=>
+                            {
+                                p.insert_pid(thread_id);
+                            },
+                            _=>()}
+                    },None=>()}
+            },None=>()};
+    }
+    pub fn get_pid(&self,processer_id:KoID)->Option<&u64>{
+        self.tid_map.get(&(processer_id as u64))
+    }
+    pub fn get_tid(&self,thread_id:KoID)->Option<&u64>{
+        self.tid_map.get(&(thread_id as u64))
     }
 }
